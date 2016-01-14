dbb75002ffbc2c2efaf7ea7e98ca75db9430573c
ouskah <ouskah.gn@gmail.com>
2016-01-14 17:08:16 +0100

    Affichage des épisodes de chaque cours

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,4 +4,16 @@ class CoursesController < ApplicationController
         @courses = Course.all
     end
 
+    def show
+    	@course = Course.find_by_slug(params[:slug])
+    		if @course.nil?
+    			redirect_to root_path
+
+    		end
+
+    end
+
+
+
+
 end
