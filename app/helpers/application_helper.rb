fdf640fc9a6445d2040a4c8683bc35593fbcfcd3
ouskah <ouskah.gn@gmail.com>
2016-01-14 13:11:10 +0100

    Cretaion helper pour ajout titre personnalisé onglet de page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	def page_title(title)
+
+		base_title = "Online Courses"
+
+		if (title.empty?)
+			return base_title
+		else
+			"#{title} - #{base_title}"
+		end
+	end
+
+
 end
