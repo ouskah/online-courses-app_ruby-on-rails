1aeff8388eab0713a340d31c34fbe0410b9ee3dc
ouskah <ouskah.gn@gmail.com>
2016-01-14 15:59:06 +0100

    Ajout relation One To Many pour lier les Models Course et Episode

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,9 @@ class Course < ActiveRecord::Base
 
 	has_attached_file :image, styles: { medium: "300x300#", thumb: "150x150#" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+
+    has_many :episodes
+
+
 end
